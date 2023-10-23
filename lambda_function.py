3ba7690cbf4e057bee8e60bc1170f1ed67e45246
aahm2221 <aisha.ahmed1@yahoo.com>
2023-10-23 15:49:59 -0500

    add path

--- lambda_function.py
+++ lambda_function.py
@@ -9,63 +9,64 @@ client = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
     try:
-        if event['queryStringParameters']['team_ids']:
-            batch_keys = {
-                os.environ['TABLE_NAME']: {
-                    'Keys': [{'PK':'Current', 'SK': ('Team#'+teamId)} for teamId in event['queryStringParameters']['team_ids']],
-                    'ProjectionExpression':'TeamId,#n,Points,#r',
-                    'ExpressionAttributeNames':{'#n': 'Name', '#r':'Rank'}
+        if event['input']['path'] == '/team_rankings':
+            if event['queryStringParameters']['team_ids']:
+                batch_keys = {
+                    os.environ['TABLE_NAME']: {
+                        'Keys': [{'PK':'Current', 'SK': ('Team#'+teamId)} for teamId in event['queryStringParameters']['team_ids']],
+                        'ProjectionExpression':'TeamId,#n,Points,#r',
+                        'ExpressionAttributeNames':{'#n': 'Name', '#r':'Rank'}
+                    }
                 }
-            }
-            response = dynamodb.batch_get_item(RequestItems=batch_keys)
-            return {
-                'statusCode': 200, 
-                'body':json.dumps(json_util.loads(response['Responses'][os.environ['TABLE_NAME']]))
-            }
-    except TypeError:
-        paginator = client.get_paginator('query')
-        try:
-            paginatorConditions ={
-                'TableName':os.environ['TABLE_NAME'],
-                'IndexName': os.environ['POINTS_LSI_NAME'],
-                'ScanIndexForward':False,
-                'ProjectionExpression':'TeamId,#n,Points,#r',
-                'KeyConditionExpression':'PK = :PK',
-                'ExpressionAttributeValues':{
-                    ':PK': {'S': 'Current'},
-                    '#n':'Name',
-                    '#r':'Rank'
-                },
-            }
-            if event['queryStringParameters']['next_token']:
+                response = dynamodb.batch_get_item(RequestItems=batch_keys)
+                return {
+                    'statusCode': 200, 
+                    'body':json.dumps(json_util.loads(response['Responses'][os.environ['TABLE_NAME']]))
+                }
+        else:
+            paginator = client.get_paginator('query')
+            try:
+                paginatorConditions ={
+                    'TableName':os.environ['TABLE_NAME'],
+                    'IndexName': os.environ['POINTS_LSI_NAME'],
+                    'ScanIndexForward':False,
+                    'ProjectionExpression':'TeamId,#n,Points,#r',
+                    'KeyConditionExpression':'PK = :PK',
+                    'ExpressionAttributeValues':{
+                        ':PK': {'S': 'Current'},
+                        '#n':'Name',
+                        '#r':'Rank'
+                    },
+                }
+                if event['queryStringParameters']['next_token']:
+                    response = paginator.paginate(
+                        paginatorConditions,
+                        PaginationConfig={
+                            'MaxItems': event['queryStringParameters']['number_of_teams'],
+                            'PageSize': event['queryStringParameters']['number_of_teams'],
+                            'StartingToken': event['queryStringParameters']['next_token']
+                        }
+                    ).build_full_result()
+                result = {
+                    'Items':response['Items'],
+                    'NextToken':response['NextToken']
+                }
+            except TypeError:
                 response = paginator.paginate(
                     paginatorConditions,
                     PaginationConfig={
                         'MaxItems': event['queryStringParameters']['number_of_teams'],
-                        'PageSize': event['queryStringParameters']['number_of_teams'],
-                        'StartingToken': event['queryStringParameters']['next_token']
+                        'PageSize': event['queryStringParameters']['number_of_teams']
                     }
                 ).build_full_result()
-            result = {
-                'Items':response['Items'],
-                'NextToken':response['NextToken']
-            }
-        except TypeError:
-            response = paginator.paginate(
-                paginatorConditions,
-                PaginationConfig={
-                    'MaxItems': event['queryStringParameters']['number_of_teams'],
-                    'PageSize': event['queryStringParameters']['number_of_teams']
+                result = {
+                    'Items':response['Items'],
+                    'NextToken':response['NextToken']
                 }
-            ).build_full_result()
-            result = {
-                'Items':response['Items'],
-                'NextToken':response['NextToken']
+            return {
+                'statusCode': 200, 
+                'body':json.dumps(json_util.loads(result))
             }
-        return {
-            'statusCode': 200, 
-            'body':json.dumps(json_util.loads(result))
-        }
     except Exception as e:
         print(f'Exception: {e}')
     return {
