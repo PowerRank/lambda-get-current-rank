f9ee1a097cde998b68380e15619a557278ee486e
Aisha Ahmed <aisha.ahmed1@yahoo.com>
2023-10-23 20:24:48 -0500

    Add functionality (#3)
    * initial implementation
    
    * fix syntax
    
    * remove reserved word
    
    * add path
    
    * add logging
    
    * edit
    
    * edit
    
    * remove conditions
    
    * add attribute names
    
    * check list
    
    * edit
    
    * deal with no next token
    
    * handle no token
    
    * cleanup
    
    * test
    
    * edit
    
    * log
    
    * fix
    
    * test
    
    * edit
    
    * cleanup
    
    * make default 20 teams returned

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,87 @@
+import boto3
 import json
+import os
+from dynamodb_json import json_util
+from boto3.dynamodb.conditions import Key
+
+dynamodb = boto3.resource('dynamodb')
+client = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
-    # TODO implement
+    try:
+        if event['path'] == '/team_rankings':
+            batch_keys = {
+                os.environ['TABLE_NAME']: {
+                    'Keys': [{'PK':'Current', 'SK': ('Team#'+teamId)} for teamId in event['queryStringParameters']['team_ids'].split(',')],
+                    'ProjectionExpression':'TeamId,#n,Points,#r',
+                    'ExpressionAttributeNames':{'#n': 'Name', '#r':'Rank'}
+                }
+            }
+            response = dynamodb.batch_get_item(RequestItems=batch_keys)
+            return {
+                'statusCode': 200, 
+                'body':json.dumps(json_util.loads(response['Responses'][os.environ['TABLE_NAME']]))
+            }
+        else:
+            paginator = client.get_paginator('query')
+            numberOfTeams = 20
+            if 'number_of_teams' in locals()['event']['queryStringParameters']:
+                numberOfTeams = event['queryStringParameters']['number_of_teams']
+            if 'next_token' in locals()['event']['queryStringParameters']:
+                response = paginator.paginate(
+                    TableName=os.environ['TABLE_NAME'],
+                    IndexName= os.environ['POINTS_LSI_NAME'],
+                    ScanIndexForward=False,
+                    ProjectionExpression='TeamId,#n,Points,#r',
+                    KeyConditionExpression='PK = :PK',
+                    ExpressionAttributeValues={
+                        ':PK': {'S': 'Current'}
+                    },
+                    ExpressionAttributeNames ={
+                        '#n':'Name',
+                        '#r':'Rank'
+                    },
+                    PaginationConfig={
+                        'MaxItems': numberOfTeams,
+                        'PageSize': numberOfTeams,
+                        'StartingToken': event['queryStringParameters']['next_token']
+                    }
+                ).build_full_result()
+            else:
+                response = paginator.paginate(
+                    TableName=os.environ['TABLE_NAME'],
+                    IndexName= os.environ['POINTS_LSI_NAME'],
+                    ScanIndexForward=False,
+                    ProjectionExpression='TeamId,#n,Points,#r',
+                    KeyConditionExpression='PK = :PK',
+                    ExpressionAttributeValues={
+                        ':PK': {'S': 'Current'}
+                    },
+                    ExpressionAttributeNames ={
+                        '#n':'Name',
+                        '#r':'Rank'
+                    },
+                    PaginationConfig={
+                        'MaxItems': numberOfTeams,
+                        'PageSize': numberOfTeams
+                    }
+                ).build_full_result()
+            if 'NextToken' in locals()['response']:
+                result = {
+                    'Items':response['Items'],
+                    'NextToken':response['NextToken']
+                }
+            else:
+                result = {
+                    'Items':response['Items']
+                }
+            return {
+                'statusCode': 200, 
+                'body':json.dumps(json_util.loads(result))
+            }
+    except Exception as e:
+        print(f'Exception: {e}')
     return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda 2!')
-    }
-
+            'statusCode': 500,
+            'body': json.dumps('Invalid Request')
+        }
